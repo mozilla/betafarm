798067d86b1f47c7d280bf9183d0be31eef5c29d
Paul Osman <paul@eval.ca>
2011-06-14 13:57:29 -0400

    Some code cleanup

---
+++ apps/users/utils.py
@@ -0,0 +1,44 @@
+from django.contrib import auth
+
+from users.forms import AuthenticationForm, RegistrationForm
+from users.models import Profile
+
+
+def handle_signin(request):
+    """Helper function that signs a user in."""
+    auth.logout(request)
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            auth.login(request, form.get_user())
+            if request.session.test_cookie_worked():
+                request.session.delete_test_cookie()
+        return form
+    request.session.set_test_cookie()
+    return AuthenticationForm()
+
+
+def handle_signup(request):
+    """Helper function for signups."""
+    if request.method == 'POST':
+        form = RegistrationForm(data=request.POST)
+        if form.is_valid():
+            profile = Profile.objects.create_inactive_user(
+                form.cleaned_data['username'],
+                form.cleaned_data['password'],
+                form.cleaned_data['email'],
+            )
+            profile.send_confirmation_email()
+        return form
+    return RegistrationForm()
+
+
+def get_next_url(request):
+    """Given a request object, return a safe next URL."""
+    gets = request.GET.copy()
+    if auth.REDIRECT_FIELD_NAME not in gets:
+        return None
+    url = gets[auth.REDIRECT_FIELD_NAME]
+    if url and '://' in url:
+        url = None
+    return url
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,52 +1,49 @@
 from django.contrib import auth
+from django.contrib import messages
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 
-from users.forms import RegistrationForm, AuthenticationForm
+from users.decorators import anonymous_only
 from users.models import Profile
+from users.utils import handle_signin, handle_signup, get_next_url
 
+from tower import ugettext as _
 import jingo
 
 
+@anonymous_only
 def signin(request):
-    form = AuthenticationForm()
-    if request.method == 'POST':
-        form = AuthenticationForm(data=request.POST)
-        if form.is_valid():
-            auth.login(request, form.get_user())
-            if request.session.test_cookie_worked():
-                request.session.delete_test_cookie()
+    """Sign the user in."""
+    form = handle_signin(request)
     if request.user.is_authenticated():
-        return HttpResponseRedirect(reverse('innovate_splash'))
+        redirect_url = get_next_url(request) or reverse('innovate_splash')
+        return HttpResponseRedirect(redirect_url)
     return jingo.render(request, 'users/signin.html', {
         'form': form,
     })
 
 
+@anonymous_only
+def signup(request):
+    """Sign up form for new users."""
+    form = handle_signup(request)
+    if form.is_valid():
+        return jingo.render(request, 'users/signup_done.html')
+    return jingo.render(request, 'users/signup.html', dict(form=form))
+
+
 def signout(request):
+    """Sign the user out, destroying their session."""
     auth.logout(request)
     return HttpResponseRedirect(reverse('innovate_splash'))
 
 
-def signup(request):
-    form = RegistrationForm()
-    if request.method == 'POST':
-        form = RegistrationForm(data=request.POST)
-        if form.is_valid():
-            profile = Profile.objects.create_inactive_user(
-                form.cleaned_data['username'],
-                form.cleaned_data['password'],
-                form.cleaned_data['email'],
-            )
-            profile.send_confirmation_email()
-            return jingo.render(request, 'users/signup_done.html')
-    return jingo.render(request, 'users/signup.html', dict(form=form))
-
-
+@anonymous_only
 def confirm(request, confirmation_token):
+    """Confirm that a user is the owner of their email address."""
     profile = get_object_or_404(Profile, confirmation_token=confirmation_token)
     profile.user.is_active = True
     profile.user.save()
-    # todo - message about user being activated
+    messages.info(request, _('Thank you, you may now log in'))
     return HttpResponseRedirect(reverse('users_signin'))
