afa169588086c291234e2755f0243ada45e7ca27
Paul Osman <paul@eval.ca>
2011-06-17 15:01:54 -0400

    Remove cruft from urls.py

--- urls.py
+++ urls.py
@@ -1,18 +1,10 @@
 from django.conf import settings
-from django.conf.urls.defaults import *
-
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.conf.urls.defaults import patterns, include
 
 urlpatterns = patterns('',
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
     (r'', include('innovate.urls')),
     (r'', include('users.urls')),
+    (r'^topics', include('topics.urls')),
 )
 
 # Handle 404 and 500 errors
