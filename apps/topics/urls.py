3a35965a27e0eb95af86c513f5159c7d9582026d
Paul Osman <paul@eval.ca>
2011-06-22 20:27:43 -0700

    Was missing a very important slash

--- apps/topics/urls.py
+++ apps/topics/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('',
     url(r'^/$', 'topics.views.all', name='topics_all'),
-    url(r'^(?P<slug>[\w-]+)/$', 'topics.views.show', name='topics_show'),
+    url(r'^/(?P<slug>[\w-]+)/$', 'topics.views.show', name='topics_show'),
 )
