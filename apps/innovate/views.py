e90346f3446384c90c18ee6af8d0c8e5fa6d01a2
Paul Osman <paul@eval.ca>
2011-06-29 17:51:04 -0400

    Show random featured user on splash page

--- apps/innovate/views.py
+++ apps/innovate/views.py
@@ -2,16 +2,19 @@ import random
 
 import jingo
 
+from users.models import Profile
 from projects.models import Project
 
 
 def splash(request):
     projects = Project.objects.filter(featured=True)
     project = projects and random.choice(projects) or None
+    users = Profile.objects.filter(featured=True)
+    user = users and random.choice(users) or None
     return jingo.render(request, 'innovate/splash.html', {
         'featured_project': project,
         'featured_event': None,
-        'featured_person': None,
+        'featured_user': user,
     })
 
 
