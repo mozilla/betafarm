813af2a9e91d527c3f9acb10d3e06ebed1a63396
Paul Osman <paul@eval.ca>
2011-06-30 14:30:46 -0400

    Move featured_image so it appears next to featured in the django admin

--- apps/users/models.py
+++ apps/users/models.py
@@ -66,9 +66,6 @@ class Profile(models.Model):
     avatar = models.ImageField(upload_to=settings.USER_AVATAR_PATH, null=True,
                                blank=True, verbose_name=_(u'Avatar'),
                                max_length=settings.MAX_FILEPATH_LENGTH)
-    featured_image = models.ImageField(verbose_name=_(u'Featured Image'),
-                                       blank=True, null=True,
-                                       upload_to=settings.USER_AVATAR_PATH)
     website = models.URLField(verbose_name=_(u'Website'), max_length=255,
                               blank=True)
     bio = models.TextField(verbose_name=_(u'Bio'), blank=True)
@@ -77,6 +74,9 @@ class Profile(models.Model):
     links = models.ManyToManyField(Link, verbose_name=_(u'Links'), blank=True)
     staff = models.BooleanField(default=False)
     featured = models.BooleanField(default=False)
+    featured_image = models.ImageField(verbose_name=_(u'Featured Image'),
+                                       blank=True, null=True,
+                                       upload_to=settings.USER_AVATAR_PATH)
 
     objects = ProfileManager()
 
