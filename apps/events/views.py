fd80d728d7c4001a7b14dca056f143d01a5fd45f
Paul Osman <paul@eval.ca>
2011-06-30 12:06:49 -0400

    Basic event functionality with ability to attend / unattend

--- apps/events/views.py
+++ apps/events/views.py
@@ -1,5 +1,11 @@
 import jingo
 
+from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404
+from django.views.decorators.http import require_POST
+
 from events.models import Event
 
 
@@ -8,3 +14,34 @@ def all(request):
     return jingo.render(request, 'events/all.html', {
         'events': events
     })
+
+
+def show(request, slug):
+    event = get_object_or_404(Event, slug=slug)
+    return jingo.render(request, 'events/show.html', {
+        'event': event
+    })
+
+
+@require_POST
+@login_required
+def attend(request, slug):
+    event = get_object_or_404(Event, slug=slug)
+    profile = request.user.get_profile()
+    if profile not in event.attendees.all():
+        event.attendees.add(profile)
+        event.save()
+    return HttpResponseRedirect(
+        reverse('events_show', kwargs=dict(slug=slug)))
+
+
+@require_POST
+@login_required
+def unattend(request, slug):
+    event = get_object_or_404(Event, slug=slug)
+    profile = request.user.get_profile()
+    if profile in event.attendees.all():
+        event.attendees.remove(profile)
+        event.save()
+    return HttpResponseRedirect(
+        reverse('events_show', kwargs=dict(slug=slug)))
