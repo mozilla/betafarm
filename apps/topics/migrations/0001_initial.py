ca412266c93b55ee9378f6fa25d1b3308fc76587
Paul Osman <paul@eval.ca>
2011-06-30 15:57:25 -0400

    Enable South migrations for topics

---
+++ apps/topics/migrations/0001_initial.py
@@ -0,0 +1,39 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Adding model 'Topic'
+        db.create_table('topics_topic', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('slug', self.gf('django.db.models.fields.SlugField')(unique=True, max_length=100, db_index=True)),
+            ('description', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('image', self.gf('django.db.models.fields.files.ImageField')(max_length=250, null=True, blank=True)),
+        ))
+        db.send_create_signal('topics', ['Topic'])
+
+
+    def backwards(self, orm):
+        
+        # Deleting model 'Topic'
+        db.delete_table('topics_topic')
+
+
+    models = {
+        'topics.topic': {
+            'Meta': {'object_name': 'Topic'},
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '250', 'null': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '100', 'db_index': 'True'})
+        }
+    }
+
+    complete_apps = ['topics']
