ca80530630d96056d3e720a919a36bd3396fe811
Paul Osman <paul@eval.ca>
2011-07-14 15:34:18 -0400

    Fix layout bug on project page and add github download / fork buttons

--- apps/projects/views.py
+++ apps/projects/views.py
@@ -16,10 +16,14 @@ def all(request):
 def show(request, slug):
     project = get_object_or_404(Project, slug=slug)
     topic = request.session.get('topic', None) or project.topics.all()[0].name
-    return jingo.render(request, 'projects/show.html', {
+    context = {
         'project': project,
         'topic': topic
-    })
+    }
+    if project.github:
+        url = project.github.rstrip('/')
+        context['download_url'] = '%s/archives/master' % (url,)
+    return jingo.render(request, 'projects/show.html', context)
 
 
 def active(request):
