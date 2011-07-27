31376048de35812343bb9231651011adbcc0c678
Paul Osman <paul@eval.ca>
2011-07-27 14:42:02 -0700

    Fixing 'active' helper to strip locales and ignore /

--- apps/innovate/helpers.py
+++ apps/innovate/helpers.py
@@ -4,11 +4,11 @@ from django.core.urlresolvers import reverse, resolve
 
 @register.function
 def active(request, url):
+    path = u'/%s' % ('/'.join(request.path.split('/')[2:]),)
     if url.endswith(reverse('projects_all')):
-        path = u'/%s' % ('/'.join(request.path.split('/')[2:]),)
         match = resolve(path)
         if match.url_name in (
             'projects_all', 'projects_show', 'projects_programs'):
             return ' selected'
-    if url.startswith(request.path):
+    if path != '/' and url.startswith(path):
         return ' selected'
