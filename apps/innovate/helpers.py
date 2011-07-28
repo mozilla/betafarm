7ee3496556c1c2f0be6bd75b1acc7fe83b621e5e
Paul Osman <paul@eval.ca>
2011-07-28 10:00:52 -0700

    Catch 404s in view helper

--- apps/innovate/helpers.py
+++ apps/innovate/helpers.py
@@ -1,14 +1,18 @@
 from jingo import register
 from django.core.urlresolvers import reverse, resolve
+from django.http import Http404
 
 
 @register.function
 def active(request, url):
     path = u'/%s' % ('/'.join(request.path.split('/')[2:]),)
     if url.endswith(reverse('projects_all')):
-        match = resolve(path)
-        if match.url_name in (
-            'projects_all', 'projects_show', 'projects_programs'):
-            return ' selected'
+        try:
+            match = resolve(path)
+            if match.url_name in (
+                'projects_all', 'projects_show', 'projects_programs'):
+                return ' selected'
+        except Http404:
+            return ''
     if path != '/' and url.startswith(path):
         return ' selected'
