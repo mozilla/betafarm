1a4c234b32bdaf36b11f62590fad0623bea920fa
Paul Osman <paul@eval.ca>
2011-08-14 23:21:21 -0400

    Surface feed entries to front page

--- apps/innovate/views.py
+++ apps/innovate/views.py
@@ -5,16 +5,19 @@ import jingo
 from users.models import Profile
 from projects.models import Project
 from events.models import Event
+from feeds.models import Entry
 
 
 def splash(request):
     def get_random(cls):
         choices = cls.objects.filter(featured=True)
         return choices and random.choice(choices) or None
+    entry = random.choice(Entry.objects.all())
     return jingo.render(request, 'innovate/splash.html', {
         'featured_project': get_random(Project),
         'featured_event': get_random(Event),
         'featured_user': get_random(Profile),
+        'entry': entry
     })
 
 
