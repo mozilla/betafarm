b449db7809d58e017fee5855080ca879bff37b92
Paul Osman <paul@eval.ca>
2011-08-15 12:27:48 -0400

    Remove unused import

--- apps/users/views.py
+++ apps/users/views.py
@@ -4,7 +4,6 @@ from django.core.paginator import Paginator
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import get_object_or_404
-from django.views.decorators.http import require_POST
 
 from activity.models import Activity
 from users.models import Profile, Link
