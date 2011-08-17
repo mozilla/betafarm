04ab41e4ed2c1c1b3e5a9ff9e7f4b01caf27a25f
Ross Bruniges <rossbruniges@gmail.com>
2011-08-17 13:21:45 +0100

    [Pivotal-16987073] Progressively enhanced loading of older activity on the blog activity pages

--- media/js/functions.js
+++ media/js/functions.js
@@ -45,16 +45,6 @@ $(document).ready(function($) {
         $(this).parents('.notification').fadeOut();
     });
 
-    // fetch a page of results and cache
-    var fetch_and_cache = function(page) {
-        $.get(document.URL + page + '/', function(data) {
-            $('<div></div>').attr('id', 'activities_page_' + page)
-                .append(data).hide().appendTo('body');
-        }).error(function() {
-            $('button.fetchActivity').detach();
-        });
-    };
-
     $('a.fetchActivity').bind('click', function() {
         var current = $(this),
             ajax_url = current.attr('href'),
@@ -93,25 +83,6 @@ $(document).ready(function($) {
             }
         });
         return false;
-    });
-
-    // fetch more activity
-    $('button.fetchActivity').bind('click', function(e) {
-        e.preventDefault();
-
-        var page = parseInt($(this).attr('page')) + 1;
-        var cached = $('#activities_page_' + page);
-
-        if (!cached.length) {
-            $.get(document.URL + page + '/', function(data) {
-                $('ul.activityStream').append(data);
-            });
-        } else {
-            $('ul.activityStream').append(cached.html());
-        }
-
-        fetch_and_cache(page + 1);
-        $('button.fetchActivity').attr('page', page);
     });
 
     $('li.links').bind('click', function(event) {
