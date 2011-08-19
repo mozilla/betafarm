4c1db8c10af775e555a3da7d9bd1234c55d63ab4
Paul Osman <paul@eval.ca>
2011-08-19 11:42:35 -0400

    Slight refactoring to innovate middleware

--- apps/innovate/middleware.py
+++ apps/innovate/middleware.py
@@ -6,22 +6,27 @@ from tower import ugettext as _
 
 
 class ProfileMiddleware(object):
+
+    @classmethod
+    def safe_paths(cls):
+        return ('users_edit', 'django.views.static.serve', 'users_signout')
+
     def process_request(self, request):
         try:
             path = u'/%s' % ('/'.join(request.path.split('/')[2:]),)
             match = resolve(path)
-            if match.url_name in ('users_edit', 'django.views.static.serve',
-                                  'users_signout'):
+            if match.url_name in ProfileMiddleware.safe_paths():
                 return
         except:
             return
         if request.user.is_authenticated():
             profile = request.user.get_profile()
-            if profile.display_name == profile.masked_email:
-                msg = _('''<strong>Almost done!</strong>
-                Just fill in a bit of profile information and
-                you\'ll be all set. At a minimum you\'ll need
-                to enter a display name so we know what to call
-                you.''')
-                messages.success(request, msg)
-                return HttpResponseRedirect(reverse('users_edit'))
+            if profile.has_chosen_identifier:
+                return
+            msg = _('''<strong>Almost done!</strong>
+            Just fill in a bit of profile information and
+            you\'ll be all set. At a minimum you\'ll need
+            to enter a display name so we know what to call
+            you.''')
+            messages.success(request, msg)
+            return HttpResponseRedirect(reverse('users_edit'))
