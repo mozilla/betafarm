825d536d730bea934443c2a0c65de6605cf1e8f6
Paul Osman <paul@eval.ca>
2011-09-07 16:37:08 -0400

    Allow users to clear uploaded pictures. Edit form now picks up gravatar URL if no uploaded picture exists.

--- apps/users/widgets.py
+++ apps/users/widgets.py
@@ -1,19 +1,15 @@
-from django.forms.widgets import FileInput
-from django.utils.html import escape
+from django.forms.widgets import ClearableFileInput
 from django.utils.safestring import mark_safe
 
 
-class ImageFileInput(FileInput):
-    template_with_initial = u'%(initial)s <br />%(input)s'
+class ImageFileInput(ClearableFileInput):
+    """
+    Simplified clearable file input. Clear checkbox will be handled in the
+    template as we have access to the model instance there.
+    """
+    template_with_initial = u'%(input)s'
 
     def render(self, name, value, attrs=None):
-        template = u'%(input)s'
-        substitutions = {
-            'input': super(ImageFileInput, self).render(
-                name, value, attrs)
-        }
-        if value and hasattr(value, 'url'):
-            template = self.template_with_initial
-            substitutions['initial'] = (u'<img src="%s">' % (
-                escape(value.url),))
-        return mark_safe(template % substitutions)
+        return mark_safe(self.template_with_initial % {
+            'input': super(ImageFileInput, self).render(name, value, attrs),
+        })
