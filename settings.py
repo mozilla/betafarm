c569587b48609801259b213c8f8ee715b3f969a2
Ross Bruniges <rossbruniges@gmail.com>
2011-09-19 17:50:06 +0100

    Removing my bodged up buildID as I see we have a helper for it in jingo-minify

--- settings.py
+++ settings.py
@@ -108,7 +108,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.messages.context_processors.messages',
 
     'commons.context_processors.i18n',
-    #'jingo_minify.helpers.build_ids',
+    'jingo_minify.helpers.build_ids',
 
     'topics.context_processors.topics',
     'django.core.context_processors.request',
