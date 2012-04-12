cc2a600a7a68e438aa1dceb43f40c7ccd61b5df9
Paul McLanahan <paul@mclanahan.net>
2012-04-12 16:57:41 -0400

    Add shift-refresh to update HP cache on demand.

--- apps/innovate/views.py
+++ apps/innovate/views.py
@@ -6,10 +6,13 @@ from projects.models import Project
 
 def splash(request):
     """Display splash page. With featured projects and news feed."""
+    # refresh cache if requested
+    force = request.META.get('HTTP_CACHE_CONTROL') == 'no-cache'
+    entries = get_blog_feed_entries(force_update=force)
     projects = Project.objects.filter(featured=True, inactive=False)[:4]
     return jingo.render(request, 'innovate/splash.html', {
         'featured_projects': projects,
-        'blog_entries': get_blog_feed_entries(),
+        'blog_entries': entries,
     })
 
 
